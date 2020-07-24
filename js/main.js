94991bdab3ea9bd7b3cf7f513383c52c4d38cb31
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2020-07-23 22:16:42 -0400

    added testimonial secion

--- js/main.js
+++ js/main.js
@@ -1,102 +1,23 @@
-var $slider = $(".slider");
-var $slides = $slider.find(".slider-item");
-var $navPrev = $(".go-prev");
-var $navNext = $(".go-next");
-var $startAutoplay = $(".start-autoplay");
-var $stopAutoplay = $(".stop-autoplay");
-var slidesNum = $slides.length;
-var prevSlideID = null;
-var currentSlideID = 0;
-var isAnimating = false;
-var isAutoPlay = false;
-
-function init() {
-    TweenLite.set($slides, {
-        left: "-100%"
+$(document).ready(function() {
+    $("#testimonial-slider").owlCarousel({
+      items: 1,
+      itemsDesktop: [1199, 1],
+      itemsDesktopSmall: [979, 1],
+      itemsTablet: [768, 1],
+      pagination: true,
+      autoPlay: true
     });
-    $navPrev.on("click", gotoPrevSlide);
-    $navNext.on("click", gotoNextSlide);
-    $startAutoplay.on("click", startAutoPlay);
-    $stopAutoplay.on("click", stopAutoPlay);
-    gotoSlide(0, 0);
-}
-
-function gotoPrevSlide() {
-    var slideToGo = currentSlideID - 1;
-    if (slideToGo <= -1) {
-        slideToGo = slidesNum - 1;
-    }
-    stopAutoPlay();
-    gotoSlide(slideToGo, 1, "prev");
-}
-
-function gotoNextSlide() {
-    var slideToGo = currentSlideID + 1;
-    if (slideToGo >= slidesNum) {
-        slideToGo = 0;
-    }
-    stopAutoPlay();
-    gotoSlide(slideToGo, 1, "next");
-}
-
-function gotoSlide(slideID, _time, _direction) {
-    if (!isAnimating) {
-        isAnimating = true;
-        prevSlideID = currentSlideID;
-        currentSlideID = slideID;
-        var $prevSlide = $slides.eq(prevSlideID);
-        var $currentSlide = $slides.eq(currentSlideID);
-        var time = 1;
-        if (_time !== null) {
-            time = _time;
-        }
-        var direction = "next";
-        if (_direction != null) {
-            direction = _direction;
-        }
-        if (direction == "next") {
-            TweenLite.to($prevSlide, time, {
-                left: "-100%"
-            });
-            TweenLite.fromTo($currentSlide, time, {
-                left: "100%"
-            }, {
-                left: "0"
-            });
-        } else {
-            TweenLite.to($prevSlide, time, {
-                left: "100%"
-            });
-            TweenLite.fromTo($currentSlide, time, {
-                left: "-100%"
-            }, {
-                left: "0"
-            });
-        }
-        TweenLite.delayedCall(time, function() {
-            isAnimating = false;
-        });
-    }
-}
-
-function play(){
-  gotoNextSlide();
-  TweenLite.delayedCall(4, play);
-}
-
-function startAutoPlay(immediate) {
-    if (immediate != null) {
-        immediate = false;
-    }
-    
-    if (immediate) {
-        gotoNextSlide();
-    }
-    TweenLite.delayedCall(4, play);
-}
-
-function stopAutoPlay() {
-  isAutoPlay = false;
-    TweenLite.killDelayedCallsTo(play);
-}
-init();
+   /* Array.prototype.slice.call(document.getElementById("testimonial-slider").children),
+    LeftArrow = document.getElementById("left-arrow"),
+    RightArrow = document.getElementById("right-arrow"),
+    Speed = 10000,
+    currentSlide = 0,
+    currentActive = 0,
+    Timer,
+		touchStartPos,
+		touchEndPos,
+		touchPosDiff,
+		ignoreTouch = 30;
+;
+*/
+  });
