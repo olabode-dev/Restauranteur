9f815dc6265523ba312c7ab6e1e07bf961b03c65
Rosemary <59313516+Nwosu-Ihueze@users.noreply.github.com>
2020-07-24 01:30:30 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $("#testimonial-slider").owlCarousel({
+      items: 1,
+      itemsDesktop: [1199, 1],
+      itemsDesktopSmall: [979, 1],
+      itemsTablet: [768, 1],
+      pagination: true,
+      autoPlay: true
+    });
+   /* Array.prototype.slice.call(document.getElementById("testimonial-slider").children),
+    LeftArrow = document.getElementById("left-arrow"),
+    RightArrow = document.getElementById("right-arrow"),
+    Speed = 10000,
+    currentSlide = 0,
+    currentActive = 0,
+    Timer,
+		touchStartPos,
+		touchEndPos,
+		touchPosDiff,
+		ignoreTouch = 30;
+;
+*/
+  });
